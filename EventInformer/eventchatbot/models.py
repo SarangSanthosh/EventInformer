ec08f91904b8e62e9569f787ba3cdf540712d1bb
Sarang <sarangsanthoshkumar@gmail.com>
2024-12-27 06:56:23 +0000

    Second Commit

--- EventInformer/eventchatbot/models.py
+++ EventInformer/eventchatbot/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=255)
+    expected_participants = models.IntegerField()
+    seats_left = models.IntegerField()
+    poster_url = models.URLField(blank=True)
+    registration_link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
