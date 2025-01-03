7b80d8c3814b9ca12a4b8cc13b87fc1f0b2ece3b
Sarang <sarangsanthoshkumar@gmail.com>
2025-01-03 15:38:41 +0000

    Seventh Commit

--- EventInformer/EventInformer/urls.py
+++ EventInformer/EventInformer/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from . import views
+from django.contrib import admin
 
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),  # Main homepage route
+     path('admin/', admin.site.urls),
     path('eventchatbot/', include('eventchatbot.urls')),  # Includes urls from the eventchatbot app
 ]
